66b8c35712de846c50115fbd5f277a9ae9ec5f6c
rcmoretto <33882498+rcmoretto@users.noreply.github.com>
2018-03-02 18:28:33 -0300

    Initial commit
    teste

---
+++ Controllers/EstacionarController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EstacionatorCore.Models;
+
+
+namespace Estacionator.Views
+{
+    public class EstacionarController : Controller
+    {
+        // GET: Estacionar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Visualizacao()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EstacionatorCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
